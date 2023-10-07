c47affb8654fe1f991fd7fed07fec67fe194e363
Reşat Berk <116816908+mathpathconsole@users.noreply.github.com>
2023-10-08 00:55:41 +0300

    Update vector fields.py
    math_vector

--- math_tools/vector fields.py
+++ math_tools/vector fields.py
@@ -6,27 +6,14 @@ from sympy import *
 
 x,y = np.meshgrid(np.linspace(-5,5,10),np.linspace(-5,5,10))
 
-def f(x='your func'):
-    #x,y = np.meshgrid(np.linspace(-5,5,10),np.linspace(-5,5,10))
-    u=np.array(i)
-    v=0
-    plt.quiver(x,y,u,v)
-    plt.title("Vector fields")
-    plt.show()
+u= x**2
+v=0
 
-f(x=x**2) #function of x^2
-    
-#x,y = np.meshgrid(np.linspace(-5,5,10),np.linspace(-5,5,10))
-#u= x**2
-#v=0
-#print(type(u))
+plt.quiver(x,y,u,v)
+plt.title("Vector fields")
+plt.show()
 
-#plt.quiver(x,y,u,v)
-#plt.title("Vector fields")
-#plt.show()
-
-#indirilmesi gereken kütüphaneler:
+#libraries:
 #pip install numpy
 #pip install matplotlib
 
-#https://krajit.github.io/sympy/vectorFields/vectorFields.html --*kaynak*
